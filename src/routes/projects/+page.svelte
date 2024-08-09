02ad62e6fa15b7bb1e21908486dc8073cd5ed56a
Joshua Niemelä <josh@jniemela.dk>
2024-08-09 23:17:44 +0200

    Added project page

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Projects - Josh Niemelä</title>
+</svelte:head>
+
+<main class="page">
+	<h1>/projects</h1>
+	<p>
+		This is the hub page for all of my projects. The majority of my projects are hosted on
+		<a
+			href="
+                                                                                   https://github.com/joshniemela"
+			>GitHub</a
+		>. The majority of my professional work is not available on GitHub, but I have included a short
+		description of them in my <a href="/cv">CV </a>. A bunch of the projects currently do not have a
+		section on this page, but I will add them as I get around to it.
+	</p>
+</main>
