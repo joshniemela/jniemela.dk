84a7e9a750c18150d23f8685f80281a0b77c5ffc
Joshua Niemelä <josh@jniemela.dk>
2024-08-14 17:44:41 +0200

    Added move api

--- src/lib/hs2048.ts
+++ src/lib/hs2048.ts
@@ -1,6 +1,7 @@
 import { spawn } from 'node:child_process';
 import type { ChildProcess } from 'node:child_process';
 import net from 'node:net';
+import { AddressInfo } from 'net';
 
 // Map of string to Process, one userid gets one game
 export const games: Map<string, Hs2048Game> = new Map();
@@ -11,11 +12,24 @@ enum ServerState {
 	STOPPED
 }
 
+enum Direction {
+	UP = 'w',
+	DOWN = 's',
+	LEFT = 'a',
+	RIGHT = 'd'
+}
+
+type Board = {
+	board: number[][];
+	score: number;
+};
+
 export class Hs2048Game {
 	#server: net.Server;
 	#process?: ChildProcess;
 	#currentState: string = '';
 	#serverState: ServerState = ServerState.STOPPED;
+	#lastActivity: number = Date.now();
 	constructor() {
 		this.#server = net.createServer((socket) => {
 			console.log('Client connected');
@@ -39,11 +53,11 @@ export class Hs2048Game {
 	async start(): Promise<void> {
 		this.#serverState = ServerState.WAITING;
 		this.#server.listen(0, () => {
-			console.log(`TCP server started on port ${this.#server.address().port}`);
+			console.log(`TCP server started on port ${(this.#server.address() as AddressInfo).port}`);
 		});
 		this.#process = spawn('./src/routes/projects/hs2048/hs2048', [
 			'-p',
-			this.#server.address().port.toString()
+			(this.#server.address() as AddressInfo).port.toString()
 		]);
 		// Wait for the server to start
 		while (this.#serverState === ServerState.WAITING) {
@@ -55,12 +69,46 @@ export class Hs2048Game {
 		return this.#currentState;
 	}
 
+	getBoard(): Board {
+		// First number is the score, then the next 16 numbers are the 4x4 board
+		const data = this.#currentState.split('\n')[0].split(' ').slice(0, 17);
+		const score = parseInt(data[0]);
+		const board = [];
+		for (let i = 1; i < data.length; i += 4) {
+			board.push(
+				data.slice(i, i + 4).map((num) => {
+					const n = parseInt(num);
+					return n === 0 ? 0 : 2 ** n;
+				})
+			);
+		}
+		return { board, score };
+	}
+
+	isInactive(): boolean {
+		// If the last activity was more than 10 minutes ago, we consider it inactive and it can be deleted
+		return Date.now() - this.#lastActivity > 600000;
+	}
+	isStopped(): boolean {
+		return this.#serverState === ServerState.STOPPED;
+	}
+
 	async move(direction: string): Promise<void> {
-		this.#process?.stdin.write(direction);
-		this.#serverState = ServerState.WAITING;
+		if (this.#serverState === ServerState.STOPPED) {
+			return;
+		}
+		// We can assert non-null since the server is running, this means a process was made.
+		this.#process!.stdin!.write(direction);
+		this.#lastActivity = Date.now();
+		if (this.#currentState === 'gameover') {
+			this.#serverState = ServerState.STOPPED;
+		} else {
+			this.#serverState = ServerState.WAITING;
+		}
+
 		// Wait for the server to respond
 		while (this.#serverState === ServerState.WAITING) {
-			await new Promise((resolve) => setTimeout(resolve, 100));
+			await new Promise((resolve) => setTimeout(resolve, 10));
 		}
 	}
 
---
+++ src/routes/api/hs2048/+server.ts
@@ -0,0 +1,41 @@
+import { json } from '@sveltejs/kit';
+import { Hs2048Game } from '$lib/hs2048';
+
+let running_games = new Map<string, Hs2048Game>();
+// occasionally poll the games and delete if inactive
+setInterval(() => {
+	for (const [key, game] of running_games) {
+		if (game.isInactive() || game.isStopped()) {
+			console.log('Game inactive, stopping:', key);
+			game.stop();
+			running_games.delete(key);
+		}
+	}
+}, 6000);
+
+export async function POST({ request, cookies }) {
+	// get userid from a cookie
+	let userId = cookies.get('uuid');
+	if (!userId) {
+		throw new Error('No userid found, this should not happen');
+	}
+	// get the game for this user
+	let game = running_games.get(userId);
+	// if the game is not found, create a new game
+	if (!game) {
+		game = new Hs2048Game();
+		running_games.set(userId, game);
+		await game.start();
+	}
+	// get the direction from the request body
+	const { direction } = await request.json();
+	if (!['w', 'a', 's', 'd', 'q'].includes(direction)) {
+		return json({ error: 'Invalid direction' }, { status: 400 });
+	}
+	console.log('Moving in direction:', direction);
+	// move the game in the specified direction
+	await game.move(direction);
+	console.log('Game state:', game.getState());
+
+	return json(game.getBoard());
+}
